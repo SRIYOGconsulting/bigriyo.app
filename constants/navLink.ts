ba1de22bbb4c2eb0816752de2f71321ce7557e64
Dipkiran Shrestha <dipkiran101@gmail.com>
2026-09-24 15:35:51 +0545

    chore: add directory to store constant values

---
+++ constants/navLink.ts
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { href: "/", title: "Home" },
+  { href: "/about", title: "About" },
+  { href: "/services", title: "Services" },
+  { href: "/feedback", title: "Feedback" },
+  { href: "/about/team", title: "Team" },
+  { href: "/contact", title: "Contact" }
+];
